c89424e4d4789ef23422c9388d314b2430f36c01
khaihoan161020 <hoan161020@gmail.com>
2022-06-13 22:29:08 +0700

    filter in user

--- src/components/User/Features/index.js
+++ src/components/User/Features/index.js
@@ -3,33 +3,45 @@ import { WrapperButtons } from './Feature.style'
 import IntlMessages from "../../../util/IntlMessages";
 import userAction from '../../../appRedux/User/actions'
 import { useDispatch } from 'react-redux';
-
-const Feature = () => {
+import { useState} from 'react'
+import { formatCountdown } from 'antd/lib/statistic/utils';
+const Feature = ({onSearch, form}) => {
     const dispatch = useDispatch();
-
+    const [modeInput, setMdoeInput] = useState(true)
     const { Option } = Select;
-    const tailLayout = {
-        wrapperCol: { offset: 4, span: 20 },
-      };
+ 
     return (
         <>
-            <Form 
-                style={{width: '100%'}}
-            >
+            
                 <Row justify='start'>
                     <Col md={16}>
                         <Row>
                             <Col md={11}>
-                                <Form.Item label={<IntlMessages id="label.customFilter"/>} name="Test" {...tailLayout}>
-                                    <Select defaultValue="lucy"  >
-                                        <Option value="Type">Type</Option>
-                                        <Option value="lucy">Mean</Option>
+                                <Form.Item label={<IntlMessages id="label.customFilter"/>} name="type" >
+                                    <Select defaultValue="Username"  onChange={(value) => {
+                                        if ( value === 'isDeleted') {
+                                            setMdoeInput(false)
+                                            form.setFieldsValue({ value: 'Active'})
+                                        }
+                                        else  {
+                                            form.setFieldsValue({ value: ''})
+                                            setMdoeInput(true)
+                                        }
+                                    }}>
+                                        <Option value="username">Username</Option>
+                                        <Option value="email">Email</Option>
+                                        <Option value="isDeleted">Status</Option>
                                     </Select>
                                 </Form.Item>
                             </Col>
-                            <Col md={{span: 11, offset: 2}} {...tailLayout}>
-                                <Form.Item label={<IntlMessages id="label.value"/>} name="">
-                                  <Input />
+                            <Col md={{span: 11, offset: 2}}>
+                                <Form.Item name="value" label={<IntlMessages  id="label.value"/>} >
+                                  { modeInput 
+                                  ? <Input /> 
+                                  : <Select defaultValue="Active" >
+                                        <Option value="Active">Active</Option>
+                                        <Option value="Deleted">Deleted</Option>
+                                    </Select>}
                                 </Form.Item>
                             </Col>
                         </Row>
@@ -41,14 +53,16 @@ const Feature = () => {
                                 onClick={() => dispatch(userAction.toggleModal())}
                             >
                                 <IntlMessages id="button.addNew"/></Button>
-                            <Button type="primary">
+                            <Button type="primary"
+                                onClick={() => onSearch()}
+                            >
                                 <IntlMessages id="button.search"/>
                             </Button>
                         </WrapperButtons>
                     
                     </Col>
                 </Row>
-            </Form>
+        
         </>
     )
 }
--- src/routes/inBuiltApps/User/index.js
+++ src/routes/inBuiltApps/User/index.js
@@ -1,30 +1,54 @@
-import React, { useState } from 'react'
-import { Row, Col, Form } from 'antd';
-import Layout from '../../../components/components/Layout'
-import Features from '../../../components/User/Features'
-import UserTable from '../../../components/User/Table'
-import UserModal from '../../../components/User/Modal'
-import { useSelector } from 'react-redux';
+import React, { useState } from "react";
+import { Row, Col, Form } from "antd";
+import Layout from "../../../components/components/Layout";
+import Features from "../../../components/User/Features";
+import UserTable from "../../../components/User/Table";
+import UserModal from "../../../components/User/Modal";
+import { useDispatch, useSelector } from "react-redux";
+import userAction from "../../../appRedux/User/actions";
 
 const User = () => {
-    const visibleModal = useSelector((state) => state.user.visibleModal)
+	const visibleModal = useSelector((state) => state.user.visibleModal);
+	const [form] = Form.useForm();
+	const dispatch = useDispatch()
+	const [params, setParams] = useState({
+		page: 1,
+		limit: 10,
+		type: "username",
+		value: "",
+	});
+  const onSearch = async () => {
+    const values = await form.validateFields();
+    console.log(values);
+    const tparams = params;
+	if (values.type) tparams.type = values.type;
+    if (values.value) {
+		tparams.value = values.value;
+		
+	}
 
-    const [params, setParams] = useState({
-        page: 1,
-        limit: 10,
-        type: 'username',
-        value: ''
-    })
-    return (
-        <Layout>
-            {visibleModal && <UserModal/>}
-            <Row>
-                <Features/>
-            </Row>
-            <Row>
-                <UserTable params={params} setParams={setParams}/>
-            </Row>
-        </Layout>
-    )
-}
+	if(values.type === 'isDeleted') {
+		if(values.value === 'Deleted')
+			tparams.value = true
+		if(values.value === 'Active')
+			tparams.value = false
+	}
+
+
+    dispatch(userAction.fetchUser(params));
+  };
+  return (
+    <Layout>
+      {visibleModal && <UserModal />}
+      <Row>
+        <Form form={form} style={{ width: "100%" }}>
+          <Features form={form} onSearch={onSearch} />
+        </Form>
+      </Row>
+      <Row>
+        <UserTable params={params} setParams={setParams} />
+      </Row>
+    </Layout>
+  );
+};
 export default User;
